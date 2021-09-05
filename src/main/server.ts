56f2baa60898c205a74d05164950e65b70fa39a8
Kalléo Pinheiro <windson.rocha@zup.com.br>
2021-09-05 13:55:00 -0300

    refactor: change library nodemon to forever

--- src/main/server.ts
+++ src/main/server.ts
@@ -3,7 +3,7 @@ import express from 'express'
 const app = express()
 
 app.get('/', (req, res) => {
-  res.json({ status: 200, message: 'Hey guys2!' })
+  res.json({ status: 200, message: 'Hey guys!' })
 })
 
 app.listen(3000, () => console.info('Server is running on port 3000'))
